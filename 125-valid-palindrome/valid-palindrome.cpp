7d574339cab784ecb89383cce60f8e587de50dea
Aditya Kumar Singh <152522954+aditya2developer@users.noreply.github.com>
2026-09-07 23:59:18 +0530

    Time: 4 ms (14.03%) | Memory: 10.2 MB (28.89%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(char c:s){
+            if(isalnum(c)){
+                ans+=tolower(c);
+            }
+        }
+        int left = 0;
+        int right = ans.size()-1;
+
+        while(left<right){
+            if(ans[left]!=ans[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
